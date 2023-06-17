69071ddb0d08cf1755dc4d420871e35d68bd0b83
Erick Garcia <eggarcia9814@gmail.com>
2023-06-16 21:43:27 -0500

    implementing test to check a list of multiples products

--- exercise_unittest.py
+++ exercise_unittest.py
@@ -20,6 +20,13 @@ class TestCalculateTotalPrice(unittest.TestCase):
         expected_total = 20
         self.assertEqual(calculate_total_price(items), expected_total)
 
+    def test_multiple_items(self):
+        items = [{'name': 'item1', 'price': 10, 'quantity': 2},
+                 {'name': 'item2', 'price': 5, 'quantity': 3},
+                 {'name': 'item3', 'price': 3, 'quantity': 1}]
+        expected_total = 34
+        self.assertEqual(calculate_total_price(items), expected_total)
+
  
 if __name__ == '__main__':
     unittest.main()
