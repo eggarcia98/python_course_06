509abdacae6ade22b740aa5bcb04ea724474d817
Erick Garcia <eggarcia9814@gmail.com>
2023-06-16 21:06:23 -0500

    implementing test to check a list of only one product

--- exercise_unittest.py
+++ exercise_unittest.py
@@ -15,7 +15,11 @@ class TestCalculateTotalPrice(unittest.TestCase):
         expected_total = 0
         self.assertEqual(calculate_total_price(items), expected_total)
 
- 
+    def test_single_item(self):
+        items = [{'name': 'item1', 'price': 10, 'quantity': 2}]
+        expected_total = 20
+        self.assertEqual(calculate_total_price(items), expected_total)
 
+ 
 if __name__ == '__main__':
     unittest.main()
